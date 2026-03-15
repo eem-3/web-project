5b1f59b9ee0af914269a6dd67fcc5414c60b055d
Keldo44 <82040605+Keldo44@users.noreply.github.com>
2026-03-16 00:25:00 +0100

    Revert "Docker base"
    This reverts commit 9135fbe02446b18cec2ca26a6c56e2c78776d118.

--- entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# entrypoint.sh
-set -e  # Si la migración falla, que el contenedor se detenga
-
-echo "Aplicando migraciones..."
-uv run python manage.py migrate
-
-echo "Iniciando servidor..."
-exec uv run python manage.py runserver 0.0.0.0:8000
